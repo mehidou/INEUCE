5784390f1820b2497b042c4f204aa5448b15540a
Michaël Bruneel <m@alethes.be>
2016-07-08 11:29:32 +0200

    Add files to complete the starter kit.
    From that revision we will merge that repository with GIT instead of
    using composer. This will be easier to maintain.

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -e "composer.phar" ]; then
+    curl -sS https://getcomposer.org/installer | php
+fi
+
+# if composer.phar is over 30 days old
+if test "`find composer.phar -mtime +30`"; then
+    php composer.phar --ansi self-update
+fi
+
+php composer.phar --ansi install
